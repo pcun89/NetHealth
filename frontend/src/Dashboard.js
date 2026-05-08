8f6b818f3fdb584d35e5ec6ea1fe7cbe77a8fc65
pcun89 <phillippcun89@gmail.com>
2026-05-08 09:50:43 -0500

    dashboard update

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import MetricsChart from "../MetricsChart";
+import io from "socket.io-client";
+import MetricsChart from "./MetricsChart";
+
+const socket = io("http://localhost:5000");
 
 function Dashboard() {
     const [metrics, setMetrics] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/api/metrics/192.168.1.1")
-            .then(res => res.json())
-            .then(data => setMetrics(data.metrics));
+        socket.on("metrics_update", (data) => {
+            setMetrics(prev => [...data, ...prev].slice(0, 50));
+        });
+
+        return () => socket.disconnect();
     }, []);
 
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Live Dashboard</h1>
             <MetricsChart data={metrics} />
         </div>
     );
